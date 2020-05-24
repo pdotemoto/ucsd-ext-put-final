c70dc8ad4b4a9e04552cd8d21e2532d45d3ecbb4
pdotemoto <43582256+pdotemoto@users.noreply.github.com>
2020-05-24 00:21:01 -0700

    Update test_counter.py
    add additional tests for negative values

--- test_counter.py
+++ test_counter.py
@@ -4,7 +4,17 @@ import pytest
 
 def test_counter_inc():
     assert counter.inc(4) == 5
+    assert counter.inc(0) == 1
+    assert counter.inc(-1) == 0
+    assert counter.inc(-2) == -1
+    assert counter.inc(99) == 100
+    assert counter.inc(999) == 1000
 
 
 def test_counter_dec():
     assert counter.dec(5) == 4
+    assert counter.dec(1) == 0
+    assert counter.dec(0) == -1
+    assert counter.dec(-1) == -2
+    assert counter.dec(100) == 99
+    assert counter.dec(1000) == 999
